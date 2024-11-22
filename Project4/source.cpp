6795272b42908df02910567dc46884dc66bd4bef
Vlad-Gabriel Bila <bila.vlad.u6i@student.ucv.ro>
2024-11-22 11:13:19 +0200

    update2

--- Project4/source.cpp
+++ Project4/source.cpp
@@ -8,6 +8,7 @@
 #define NUM_PROCESSES 10
 #define RANGE 1000
 
+// Function to check if a number is prime
 bool isPrime(int num) {
     if (num < 2) return false;
     for (int i = 2; i * i <= num; ++i) {
@@ -16,6 +17,7 @@ bool isPrime(int num) {
     return true;
 }
 
+// Child process function to find primes in a range and write them to a pipe
 void findPrimesInRange(int start, int end, int writePipe) {
     std::ostringstream resultStream;
     for (int i = start; i < end; ++i) {
@@ -25,32 +27,34 @@ void findPrimesInRange(int start, int end, int writePipe) {
     }
     std::string result = resultStream.str();
     write(writePipe, result.c_str(), result.size());
-    close(writePipe);
+    close(writePipe); // Ensure the pipe is closed after writing
     _exit(0); // Terminate the child process
 }
 
+// Process 1 collects results from all other processes and sends to the parent
 void processOne(int pipes[NUM_PROCESSES][2], int writeToParent) {
     std::ostringstream aggregatedResults;
 
     // Process 1 collects results from other child processes
     for (int i = 1; i < NUM_PROCESSES; ++i) {
+        close(pipes[i][1]); // Close the write end of the pipe
         char buffer[1024];
         ssize_t bytesRead;
-        close(pipes[i][1]); // Close write end of the pipe
         while ((bytesRead = read(pipes[i][0], buffer, sizeof(buffer) - 1)) > 0) {
-            buffer[bytesRead] = '\0';
+            buffer[bytesRead] = '\0'; // Null-terminate the string
             aggregatedResults << buffer;
         }
-        close(pipes[i][0]); // Close read end after reading
+        close(pipes[i][0]); // Close the read end after reading
     }
 
     // Send the aggregated results to the parent
     std::string finalOutput = aggregatedResults.str();
     write(writeToParent, finalOutput.c_str(), finalOutput.size());
-    close(writeToParent);
+    close(writeToParent); // Close the pipe to parent
     _exit(0); // Terminate Process 1
 }
 
+// Function to create child processes and manage pipes
 void createProcesses() {
     int pipes[NUM_PROCESSES][2]; // Pipes for communication between parent and children
     int pipeToParent[2];         // Pipe for Process 1 to send results to the parent
@@ -73,7 +77,7 @@ void createProcesses() {
             exit(1);
         }
 
-        if (pids[i] == 0) { // Child processes
+        if (pids[i] == 0) { // Child process
             if (i == 0) { // Process 1
                 close(pipeToParent[0]); // Close read end of parent pipe
                 processOne(pipes, pipeToParent[1]);
@@ -99,10 +103,10 @@ void createProcesses() {
     ssize_t bytesRead;
     std::cout << "Final primes collected by Process 1:\n";
     while ((bytesRead = read(pipeToParent[0], buffer, sizeof(buffer) - 1)) > 0) {
-        buffer[bytesRead] = '\0';
-        std::cout << buffer;
+        buffer[bytesRead] = '\0'; // Null-terminate the string
+        std::cout << buffer; // Print the output as it is received
     }
-    close(pipeToParent[0]);
+    close(pipeToParent[0]); // Close the read end of the pipe
 
     // Wait for all child processes to finish
     for (int i = 0; i < NUM_PROCESSES; ++i) {
